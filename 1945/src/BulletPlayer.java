f1416d853e523d6fcf4ca1421c0ba3571fc1f9af
Quang Đại <quangdai.trinh@gmail.com>
2016-03-11 22:15:50 +0700

    Added files via upload

--- 1945/src/BulletPlayer.java
+++ 1945/src/BulletPlayer.java
@@ -8,13 +8,26 @@ import java.util.Vector;
  * Created by Quang Đại on 10/3/2016.
  */
 public class BulletPlayer extends Bullet {
-    private Vector<PlaneEnemy> planeEnemyVector = new Vector<>();
+    private Vector<PlaneEnemy> planeEnemyVector = PlaneEnemyManager.getInstance().getPlaneEnemyVector();
 
     public BulletPlayer(int positionX, int positionY, int speed) {
         super(positionX, positionY, speed);
         try {
             this.sprite = ImageIO.read(new File("Resources/Dan2.png"));
         } catch (IOException e) {
+        }
+    }
+
+    public void checkCollision() {
+        Rectangle rectBullet = new Rectangle((int) positionX, (int) positionY, sprite.getWidth(), sprite.getHeight());
+        for (int i = 0; i < planeEnemyVector.size(); i++) {
+            Rectangle rectPlaneEnemy = new Rectangle((int) planeEnemyVector.get(i).getPositionX()
+                    , (int) planeEnemyVector.get(i).getPositionY()
+                    , planeEnemyVector.get(i).getSprite().getWidth()
+                    , planeEnemyVector.get(i).getSprite().getHeight());
+            if (rectBullet.intersects(rectPlaneEnemy)) {
+                planeEnemyVector.remove(i);
+            }
         }
     }
 
@@ -26,6 +39,7 @@ public class BulletPlayer extends Bullet {
     @Override
     public void update() {
         this.move();
+        checkCollision();
     }
 
     @Override
